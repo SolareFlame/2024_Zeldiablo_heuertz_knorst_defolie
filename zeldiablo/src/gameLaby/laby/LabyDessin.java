b2cce62b0d4049af4b6c670307088181706db5d8
SolareFlame <zeldac332@gmail.com>
2024-06-05 15:24:37 +0200

    LabyDessin.java -> Added loading file

--- zeldiablo/src/gameLaby/laby/LabyDessin.java
+++ zeldiablo/src/gameLaby/laby/LabyDessin.java
@@ -2,9 +2,13 @@ package gameLaby.laby;
 
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import moteurJeu.DessinJeu;
 import moteurJeu.Jeu;
+
+import java.io.File;
 
 /**
  * Classe LabyDessin
@@ -12,6 +16,18 @@ import moteurJeu.Jeu;
 public class LabyDessin implements DessinJeu {
 
     public static final int TAILLE = 50;
+
+    public static final String PATH = "zeldiablo/textures/";
+
+    public static final String GAUCHE = PATH + "pj_left.png";
+    public static final String DROITE = PATH + "pj_right.png";
+    public static final String HAUT = PATH + "pj_up.png";
+    public static final String DOWN = PATH + "pj_down.png";
+
+
+
+
+
 
     /**
      *
@@ -24,56 +40,97 @@ public class LabyDessin implements DessinJeu {
 
         final GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        // fond
+        /*
+        -------------------- SOL --------------------
+         */
         gc.setFill(Color.DARKGREEN);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
-        // SOL
-        gc.setFill(Color.rgb(30, 30, 30));
+        /*
+        -------------------- MURS --------------------
+         */
+
         Labyrinthe laby = labyrinthe.getLabyrinthe();
 
-        // MURS
+        //MUR FACE
+        gc.setFill(Color.GRAY);
         for (int j = 0; j < laby.getLength(); j++) {
             for (int i = 0; i < laby.getLengthY(); i++) {
                 if (laby.getMur(j, i)) {
-                    gc.setFill(Color.GRAY);
                     gc.fillRect(j * TAILLE, i * TAILLE, TAILLE, TAILLE);
                 }
             }
         }
 
-        // perso
-        double persox = labyrinthe.getLabyrinthe().pj.getX();
-        double persoy = labyrinthe.getLabyrinthe().pj.getY();
 
-        gc.setFill(Color.RED);
-        gc.fillOval(persox * TAILLE, persoy * TAILLE, TAILLE, TAILLE);
+        /*
+        -------------------- PLAYER --------------------
+         */
 
-        // monstre
-        if (labyrinthe.getLabyrinthe().monstres != null) {
-            for (Monstre monstre : laby.monstres) {
-                double monstrex = monstre.getX();
-                double monstrey = monstre.getY();
-                gc.setFill(Color.rgb(127, 0, 255));
-                gc.fillOval(monstrex * TAILLE, monstrey * TAILLE, TAILLE, TAILLE);
-            }
+        double pj_x = labyrinthe.getLabyrinthe().pj.getX();
+        double pj_y = labyrinthe.getLabyrinthe().pj.getY();
+
+        String direction = GAUCHE; //temp
+
+
+        try {
+            chargerImage(gc, pj_x, pj_y, direction);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
 
 
-        //TOP MUR
+
+
+
+
+
+
+        /*
+        -------------------- TOP --------------------
+         */
+        gc.setFill(Color.BLACK);
         for (int j = 0; j < laby.getLength(); j++) {
             for (int i = 0; i < laby.getLengthY(); i++) {
                 if (laby.getMur(j, i)) {
-                    // Pour le mur du haut
                     if (i > 0 && !laby.getMur(j, i-1)) {
-                        gc.setFill(Color.BLACK);
                         gc.fillRect(j * TAILLE, (i * TAILLE)-TAILLE/2, TAILLE, TAILLE/2);
                     } else {
-                        gc.setFill(Color.BLACK);
                         gc.fillRect(j * TAILLE, (i * TAILLE)-TAILLE, TAILLE, TAILLE);
                     }
                 }
             }
         }
+
+
+
+
+
+
+
+
+
+
+        // monstre
+        if (labyrinthe.getLabyrinthe().monstre != null) {
+            double monstrex = labyrinthe.getLabyrinthe().monstre.getX();
+            double monstrey = labyrinthe.getLabyrinthe().monstre.getY();
+            gc.setFill(Color.rgb(127,0,255));
+            gc.fillOval(monstrex * TAILLE, monstrey * TAILLE, TAILLE, TAILLE);
+        }
+
+
+
+
+
+
+    }
+
+    public void chargerImage(GraphicsContext gc, double x, double y, String path) throws Exception {
+        File imageFile = new File(path);
+        String abs_path = imageFile.getAbsolutePath();
+
+        Image img = new Image(abs_path);
+        gc.drawImage(img, x * TAILLE, y * TAILLE, TAILLE, TAILLE);
     }
 }
